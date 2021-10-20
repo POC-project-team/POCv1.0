2ba8625f9beecfbb97800552a44564f2f03bf619
= <sartakov.sasha@icloud.com>
2021-10-20 09:19:37 +0700

    new project

--- New test project/Assets/Scripts/DrawCards.cs
+++ New test project/Assets/Scripts/DrawCards.cs
@@ -1,31 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DrawCards : MonoBehaviour
+public class DrawCards : NetworkBehaviour
 {
-    public GameObject Card1;
-    public GameObject Card2;
-    public GameObject PlayerArea;
-    public GameObject EnemyArea;
+    public PlayerManager PlayerManager;
 
-    List <GameObject> cards = new List <GameObject>();
-
-
-    void Start()
-    {
-        cards.Add(Card1);
-        cards.Add(Card2);    
-    }
     public void OnCLick()
     {
-        for (var i = 0; i < 5; i++) 
-        {
-            GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
-            playerCard.transform.SetParent(PlayerArea.transform, false);
-
-            GameObject enemyCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0,0,0), Quaternion.identity);
-            enemyCard.transform.SetParent(EnemyArea.transform, false);
-        }
+        NetworkIdentity NetworkIdentity = NetworkClient.connection.identity;
+        PlayerManager = NetworkIdentity.GetComponent<PlayerManager>();
+        PlayerManager.CmdDealCards();
     }
 }
