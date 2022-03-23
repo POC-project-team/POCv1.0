e785cb8c3f51a7d41e05b2f81fc4c9a25985d7f3
L3XxXa <alexkkk@inbox.ru>
2022-03-23 17:29:09 +0700

    Registration screen

--- AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/MainActivity.java
+++ AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/MainActivity.java
@@ -1,39 +1,14 @@
 package com.example.anothernfcapp;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.nfc.tech.NdefFormatable;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.TextHttpResponseHandler;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class MainActivity extends AppCompatActivity {
     EditText login;
@@ -41,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private final String LOGIN = "admin";
     private final String PASSWORD = "admin";
     Button buttonLogin;
+    Button buttonRegistarte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 loginApp();
             }
         });
+        buttonRegistarte = (Button) findViewById(R.id.registerButton);
+        buttonRegistarte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
     }
 
     private void loginApp() {
@@ -64,6 +47,11 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(this, "Wrong login or password", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void register(){
+        Intent registration = new Intent(this, RegisterUser.class);
+        startActivity(registration);
     }
 
 }
---
+++ AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/RegisterUser.java
@@ -0,0 +1,49 @@
+package com.example.anothernfcapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class RegisterUser extends Activity {
+    EditText login;
+    EditText password;
+    Button register;
+    Button goBack;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_screen);
+        login = (EditText) findViewById(R.id.loginReg);
+        password = (EditText) findViewById(R.id.passwordReg);
+        register = (Button) findViewById(R.id.sendRegistration);
+        goBack = (Button) findViewById(R.id.backToLogin);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setGoBack();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRegistration();
+            }
+        });
+    }
+
+    private void sendRegistration() {
+        Toast.makeText(this, "WIP", Toast.LENGTH_SHORT).show();
+        
+    }
+
+    private void setGoBack(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
