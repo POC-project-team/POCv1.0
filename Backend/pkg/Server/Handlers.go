31d8f176575d7f2beb710760420edc637a4e33dc
= <sartakov.sasha@icloud.com>
2022-04-07 15:44:33 +0700

    something

--- Backend/pkg/Server/Handlers.go
+++ Backend/pkg/Server/Handlers.go
@@ -2,8 +2,10 @@
 package server
 
 import (
+	"backend/pkg/APIerror"
 	service "backend/pkg/Service"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 // MyHandler defines the routes, returns router
@@ -19,7 +21,16 @@ func MyHandler() *mux.Router {
 	router.HandleFunc("/{user_id:[0-9]+}/getTags", srv.GetAllTags).Methods("GET")
 	router.HandleFunc("/{user_id:[0-9]+}/getNotes", srv.GetNotes).Methods("POST")
 	router.HandleFunc("/{user_id:[0-9]+}/addNote", srv.AddNote).Methods("POST")
+	router.HandleFunc("/test", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusCreated)
+		if _, err := writer.Write([]byte("Hello, I'm working")); err != nil {
+			APIerror.HTTPErrorHandle(writer, APIerror.HTTPErrorHandler{
+				ErrorCode:   http.StatusBadRequest,
+				Description: "I don't know…",
+			})
+		}
+		return
+	})
 	router.Handle("/", router)
-
 	return router
 }
