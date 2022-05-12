06e97d8fee77d287caf12056f3ba227254ff0bcc
L3XxXa <alexkkk@inbox.ru>
2022-05-12 09:58:38 +0700

    something

--- AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/caching/CacheForPostNotes.java
+++ AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/caching/CacheForPostNotes.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CacheForPostNotes {
     private Context context;
@@ -12,13 +14,15 @@ public class CacheForPostNotes {
     private final File cacheFilePostNotes;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
+
     public CacheForPostNotes(Context context){
         this.context = context;
         cacheFilePostNotes = new File(context.getCacheDir(), filename);
     }
 
-    public void writeToCache(String data){
-
+    public void writeToCache(String data) throws IOException {
+        bufferedWriter = new BufferedWriter(new FileWriter(filename));
+        bufferedWriter.append(data);
     }
 
 }
