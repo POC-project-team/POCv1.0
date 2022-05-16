d199011fbb1d758f7be471fa50a7ea80821ee354
L3XxXa <alexkkk@inbox.ru>
2022-05-16 08:54:36 +0700

    checking front by bubou

--- AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/screens/Settings.java
+++ AnotherNFCapp/app/src/main/java/com/example/anothernfcapp/screens/Settings.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
