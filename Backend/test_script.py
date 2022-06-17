a46d8042cb463506a9d2ec0ac9224a898ccd35c9
= <sartakov.sasha@icloud.com>
2022-06-18 01:09:20 +0700

    commit

--- Backend/test_script.py
+++ Backend/test_script.py
@@ -11,20 +11,20 @@ def main():
     for _ in range (100):
         login = random_string()
         password = login
-        auth = r.post(url='http://217.25.88.71:60494/signup', json={'login': login, 'password': password})
+        r.post(url='http://217.25.88.71:60494/signup', json={'login': login, 'password': password})
         token = r.post(url='http://217.25.88.71:60494/auth', json={'login': login, 'password': password})
         print(token.json()['token'])
 
-        tagName = random_string()
+        tag_name = random_string()
 
-        create_link = f"http://217.25.88.71:60494/{token.json()['token']}/{tagName}/tag"
+        create_link = f"http://217.25.88.71:60494/{token.json()['token']}/{tag_name}/tag"
 
         create = r.post(url=create_link, json={'tagName': "test"})
         print(create.json())
 
-        add_note = f"http://217.25.88.71:60494/{token.json()['token']}/{tagName}/note"
-        add_note_request = r.post(url=add_note, json={'note': "test"})
-        add_note_request = r.post(url=add_note, json={'note': "test123"})
+        add_note = f"http://217.25.88.71:60494/{token.json()['token']}/{tag_name}/note"
+        r.post(url=add_note, json={'note': "test"})
+        r.post(url=add_note, json={'note': "test123"})
 
 
 if __name__ == '__main__':
